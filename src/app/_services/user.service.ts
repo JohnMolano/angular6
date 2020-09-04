d3ea817e8285c90000ee1580381e6710fd116324
JohnMolano <jhon.molano7@gmail.com>
2020-09-03 19:34:13 -0500

    first commit

---
+++ src/app/_services/user.service.ts
@@ -0,0 +1,41 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { User } from '../_models';
+
+@Injectable()
+export class UserService {
+    constructor(private http: HttpClient) { }
+
+    getAll() {
+        return this.http.get<User[]>(`${config.apiUrl}/users`);
+    }
+
+    getUserBookings(user: any) {
+        let headers = new HttpHeaders()
+        .set('Content-Type', 'application/json')
+        .set('Accept', 'application/json')
+        .set('App', 'APP_BCK')
+        .set('email', 'contacto@tuten.cl') 
+        .set('adminemail', user.email)
+        .set('token', user.sessionTokenBck); 
+
+        return this.http.get<any>(`${config.apiUrl}/user/contacto%40tuten.cl/bookings?current=true`,{'headers': headers });
+    }
+
+    getById(id: number) {
+        return this.http.get(`${config.apiUrl}/users/` + id);
+    }
+
+    register(user: User) {
+        return this.http.post(`${config.apiUrl}/users/register`, user);
+    }
+
+    update(user: User) {
+        return this.http.put(`${config.apiUrl}/users/` + user.id, user);
+    }
+
+    delete(id: number) {
+        return this.http.delete(`${config.apiUrl}/users/` + id);
+    }
+}
